05deb322d07c04bef199e2f7e308c2b77de39c1e
yxq <1062888640@qq.com>
2021-09-29 18:06:21 +0800

    添加uuid cookie

--- src/core/client.ts
+++ src/core/client.ts
@@ -1,11 +1,35 @@
 import request from "./request";
+import Cookie from "js-cookie";
+import { v4 as uuid } from "uuid";
+
+import { CookieModel } from "../types/cookie";
+
 export default class Client {
   constructor(options: any) {
     this.setup();
   }
 
   private setup() {
+    this.setCookies();
     this.watch();
+  }
+
+  private setCookies() {
+    const key = "onelog";
+    const current = new Date();
+    const expires = { expires: 50 * 365 };
+    if (Cookie.get(key)) {
+      const model: CookieModel = JSON.parse(Cookie.get(key));
+      model.last_time = current.valueOf();
+      Cookie.set(key, JSON.stringify(model), expires);
+    } else {
+      const model: CookieModel = {
+        id: uuid(),
+        last_time: current.valueOf(),
+        first_time: current.valueOf(),
+      };
+      Cookie.set(key, JSON.stringify(model), expires);
+    }
   }
 
   // 监听pushState跳转
